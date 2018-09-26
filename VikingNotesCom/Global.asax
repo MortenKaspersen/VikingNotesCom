60d1fb452bcd06bf5e126a0ae5ed0ec01b067d90
MortenKaspersen <mortenps@hotmail.com>
2018-09-26 09:17:00 +0200

    Add project files.

---
+++ VikingNotesCom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VikingNotesCom.MvcApplication" Language="C#" %>
---
+++ VikingNotesCom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VikingNotesCom.Startup))]
+namespace VikingNotesCom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
